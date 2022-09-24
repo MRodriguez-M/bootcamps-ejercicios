201af4d0a7f464a92a7582ed1ef01b4408d41108
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2022-09-23 19:17:27 -0500

    Solution WIP, Unidimensional ordenamiento method passing unit test

--- src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
@@ -5,6 +5,17 @@ import com.oracle.arrays.Unidimensional;
 public class UnidimensionalImpl implements Unidimensional {
     @Override
     public void ordenamiento(int[] arr) {
+        int tempNum = 0;
+        
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[j] < arr[i]) {
+                    tempNum = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = tempNum;
+                }
+            }
+        }
     }
 
     @Override
